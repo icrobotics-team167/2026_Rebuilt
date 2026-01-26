bdbb44e94ee4bbba11681133ef49fc266dc0696d
Tommy G <thgao29@icstudents.org>
2026-01-25 19:23:07 -0600

    Didnt save previously recommit

---
+++ src/main/java/frc/cotc/Autos.java
@@ -0,0 +1,23 @@
+package frc.cotc;
+
+import choreo.auto.AutoFactory;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.cotc.swerve.Swerve;
+
+public class Autos {
+    private final AutoFactory autoFactory;
+
+    public Autos(Swerve swerve) {
+        autoFactory = new AutoFactory(swerve::getPose, 
+                                      swerve::resetPose, 
+                                      swerve::followPath,
+                                    true, 
+                                    swerve);
+    }
+
+    public Command pickupAndScoreAuto() {
+        //placeholder
+        return Commands.sequence();
+    }
+}
