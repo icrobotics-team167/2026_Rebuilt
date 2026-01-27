a25b08c8a9f32aeb4f2e8264f7ff229276b0b4ac
Jerry <jeli26@icstudents.org>
2026-01-26 19:42:41 -0600

    Misc. changes for motor, encoder, statussignal initialization
    laptop is dying

--- src/main/java/frc/cotc/shooter/HoodIOPhoenix.java
+++ src/main/java/frc/cotc/shooter/HoodIOPhoenix.java
@@ -1,26 +1,36 @@
 package frc.cotc.shooter;
 
 import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.math.util.Units;
+import frc.cotc.swerve.TunerConstants;
 
 public class HoodIOPhoenix implements HoodIO {
 
     private final TalonFX motor;
 
+    private final int HOOD_MOTOR_ID = 0; // TODO: Get the actual ID
+    private final int HOOD_ENCODER_ID = 0; // TODO: Get the actual ID
+
     private final BaseStatusSignal posSignal, velSignal, statorSignal, supplySignal;
 
     public HoodIOPhoenix() {
-        motor = new TalonFX(0); // TODO: Get the actual ID
-        var encoder = new CANcoder(0); // TODO: Get the actual ID
+        motor = new TalonFX(HOOD_MOTOR_ID); 
+        CANcoder encoder = new CANcoder(HOOD_ENCODER_ID); 
 
-        posSignal = encoder.getAbsolutePosition();
-        velSignal = motor.getVelocity();
-        statorSignal = motor.getStatorCurrent();
-        supplySignal = motor.getSupplyCurrent();
+
+
+        var encoderConfig = new CANcoderConfiguration();
+        encoder.getConfigurator().apply(encoderConfig);
+
+        posSignal = encoder.getAbsolutePosition(false);
+        velSignal = motor.getVelocity(false);
+        statorSignal = motor.getStatorCurrent(false);
+        supplySignal = motor.getSupplyCurrent(false);
     }
 
     @Override
