f686128424e0cf1db9be17312d875996619de0a3
Tommy G <thgao29@icstudents.org>
2026-02-02 19:59:29 -0600

    base ting not done

--- src/main/java/frc/cotc/shooter/TurretIOSim.java
+++ src/main/java/frc/cotc/shooter/TurretIOSim.java
@@ -1,5 +1,26 @@
+// Copyright (c) 2026 FRC 167
+// https://github.com/icrobotics-team167
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.cotc.shooter;
 
-public class TurretIOSim implements TurretIO{
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class TurretIOSim implements TurretIO {
+    private final DCMotorSim sim = 
+        new DCMotorSim(LinearSystemId.createDCMotorSystem(DCMotor.getKrakenX44(1), .25, 30),
+            DCMotor.getKrakenX44(1));
     
+    private final PIDController pid = new PIDController(1, 0, 1); // Placeholder values
+    
+    @Override
+    public void updateInputs(TurretIOInputs inputs) {
+        
+    }
 }
