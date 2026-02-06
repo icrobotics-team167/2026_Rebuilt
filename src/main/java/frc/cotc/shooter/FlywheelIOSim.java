1d8e985405657faa5efa76171d76141ff04e621f
Tommy G <thgao29@icstudents.org>
2026-02-05 19:12:18 -0600

    Created flywheeliosim

--- src/main/java/frc/cotc/shooter/FlywheelIOSim.java
+++ src/main/java/frc/cotc/shooter/FlywheelIOSim.java
@@ -1,3 +1,42 @@
-public class FlywheelIOSim {
-    
+// Copyright (c) 2026 FRC 167
+// https://github.com/icrobotics-team167
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package frc.cotc.shooter;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+import frc.cotc.Robot;
+
+public class FlywheelIOSim implements FlywheelIO {
+  private final FlywheelSim sim =
+      new FlywheelSim(
+          LinearSystemId.createFlywheelSystem(DCMotor.getKrakenX44(2), .25, 30),
+          DCMotor.getKrakenX44(2));
+
+  private final PIDController pid = new PIDController(1, 0, 1); // Placeholders
+
+  @Override
+  public void stop() {
+    sim.setInputVoltage(0);
+  }
+
+  @Override
+  public void updateInputs(FlywheelIOInputs inputs) {
+    sim.update(Robot.defaultPeriodSecs);
+
+    inputs.projectileVelMetersPerSec = sim.getAngularVelocityRadPerSec();
+    inputs.appliedVolts = sim.getInputVoltage();
+  }
+
+  @Override
+  public void runVel(double projectileVelMetersPerSec) {
+    sim.setInputVoltage(
+        pid.calculate(sim.getAngularVelocityRadPerSec(), projectileVelMetersPerSec));
+  }
 }
