289dbb7d7d70c3e5dfdacb5fa7bcd3c63e680dae
mertnince-arch <mertnince@gmail.com>
2026-02-12 18:22:30 -0600

    I built the robot code.

--- src/main/java/frc/cotc/vision/AprilTagPoseEstimator.java
+++ src/main/java/frc/cotc/vision/AprilTagPoseEstimator.java
@@ -5,7 +5,7 @@
 // license that can be found in the LICENSE file at
 // the root directory of this project.
 
-package frc.cotc.vision; 
+package frc.cotc.vision;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
