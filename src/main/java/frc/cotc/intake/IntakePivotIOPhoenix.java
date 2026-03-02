da3009435894464119cdde12e97133ec2be17069
ハイドラ <thecomputer314@gmail.com>
2026-03-01 20:34:00 -0600

    Replace the CANCoder on the intake pivot with a REV MAXSpline encoder

--- src/main/java/frc/cotc/intake/IntakePivotIOPhoenix.java
+++ src/main/java/frc/cotc/intake/IntakePivotIOPhoenix.java
@@ -10,29 +10,26 @@ package frc.cotc.intake;
 import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.PositionVoltage;
-import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
+import com.revrobotics.encoder.SplineEncoder;
 import edu.wpi.first.math.util.Units;
 import frc.cotc.Robot;
 
 public class IntakePivotIOPhoenix implements IntakePivotIO {
-  private final CANcoder encoder;
+  private final SplineEncoder encoder;
   private final TalonFX intakePivotMotor1;
   private final TalonFX intakePivotMotor2;
   private final int ENCODER_ID = 2; // Placeholder ID for encoder
+  private final double ENCODER_OFFSET_ROTATIONS = 0.0;
   private final int INTAKE_ID_1 = 0; // Placeholder ID for intakeMotor1
   private final int INTAKE_ID_2 = 1; // Placeholder ID for intakeMotor2
   private final double INTAKE_DEFAULT_VOLTAGE = 12.0;
 
-  private final BaseStatusSignal posSignal,
-      statorSignal1,
-      supplySignal1,
-      statorSignal2,
-      supplySignal2;
+  private final BaseStatusSignal statorSignal1, supplySignal1, statorSignal2, supplySignal2;
 
   public IntakePivotIOPhoenix() {
-    encoder = new CANcoder(ENCODER_ID);
+    encoder = new SplineEncoder(ENCODER_ID);
 
     intakePivotMotor1 = new TalonFX(INTAKE_ID_1);
     intakePivotMotor2 = new TalonFX(INTAKE_ID_2);
@@ -45,15 +42,16 @@ public class IntakePivotIOPhoenix implements IntakePivotIO {
     config.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     intakePivotMotor2.getConfigurator().apply(config);
 
-    posSignal = encoder.getAbsolutePosition(false);
     statorSignal1 = intakePivotMotor2.getStatorCurrent(false);
     statorSignal2 = intakePivotMotor2.getStatorCurrent(false);
     supplySignal1 = intakePivotMotor2.getSupplyCurrent(false);
     supplySignal2 = intakePivotMotor2.getSupplyCurrent(false);
     BaseStatusSignal.setUpdateFrequencyForAll(
-        50, posSignal, statorSignal1, statorSignal2, supplySignal1, supplySignal2);
-    Robot.canivoreSignals.addSignals(
-        posSignal, statorSignal1, statorSignal2, supplySignal1, supplySignal2);
+        50, statorSignal1, statorSignal2, supplySignal1, supplySignal2);
+    Robot.canivoreSignals.addSignals(statorSignal1, statorSignal2, supplySignal1, supplySignal2);
+
+    intakePivotMotor1.setPosition(encoder.getRawAngle() - ENCODER_OFFSET_ROTATIONS);
+    intakePivotMotor2.setPosition(encoder.getRawAngle() - ENCODER_OFFSET_ROTATIONS);
   }
 
   private final PositionVoltage controlSignal = new PositionVoltage(0);
@@ -70,7 +68,7 @@ public class IntakePivotIOPhoenix implements IntakePivotIO {
     inputs.statorCurrentAmps2 = statorSignal2.getValueAsDouble();
     inputs.supplyCurrentAmps1 = supplySignal1.getValueAsDouble();
     inputs.supplyCurrentAmps2 = supplySignal2.getValueAsDouble();
-    inputs.thetaRad = Units.rotationsToRadians(posSignal.getValueAsDouble());
+    inputs.thetaRad = Units.rotationsToRadians(encoder.getRawAngle() - ENCODER_OFFSET_ROTATIONS);
   }
 
   @Override
