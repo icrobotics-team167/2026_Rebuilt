986fc93b292a3735d6bca562be55db97be05e598
ハイドラ <thecomputer314@gmail.com>
2026-04-10 22:15:00 -0500

    Formatting

--- src/main/java/frc/cotc/shooter/FlywheelIOPhoenix.java
+++ src/main/java/frc/cotc/shooter/FlywheelIOPhoenix.java
@@ -11,7 +11,6 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.CoastOut;
 import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
-import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
